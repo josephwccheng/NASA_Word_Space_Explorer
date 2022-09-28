56fd814e890282400ea129914d0f3b454f783fa3
josephwccheng <joseph.wc.cheng@gmail.com>
2022-09-28 20:21:19 +1000

    added fields and ntrs results

--- build_corpus.py
+++ build_corpus.py
@@ -22,11 +22,20 @@ def process_citations_search_result(result):
         'createDate': result['created'].split('T')[0],
         'publicationDate': result['publications'][0]['publicationDate'].split('T')[0],
         'downloads_pdf': "",
-        'center': result['center']['code']
+        'center': result['center']['code'],
+        'copyright': result['copyright']['determinationType'],
+        'related': ""
     }
     
     if result['downloadsAvailable'] and len(result['downloads']) > 0:
         output['downloads_pdf'] = result['downloads'][0]['links']['pdf']
+
+    related_list = []
+    if len(result['related']) > 0:
+        for related in result['related']:
+            if 'id' in list(related.keys()):
+                related_list.append(related['id'])
+    output['related'] = ','.join(map(str, related_list))
 
     return output
 
@@ -39,7 +48,7 @@ if __name__ == '__main__':
         {"center": "CDMS", "highlight": True})
     total = int(response['stats']['total'] / 1000)
     output_ntrs_cdms_results = []
-    output_ntrs_cdms_results_path = "data/pdf/ntrs_cdms_results.csv"
+    output_ntrs_cdms_results_path = "data/ntrs_cdms_results.csv"
 
     for page_index in tqdm(range(0, total, page_size), desc="1.1. Sending through mutiple API requests to obtain document metadata"):
         if page_index + page_size > total:
@@ -49,7 +58,6 @@ if __name__ == '__main__':
         results = response['results']
         for result in results:
             output_ntrs_cdms_results.append(process_citations_search_result(result))
-
 
     with open(output_ntrs_cdms_results_path, 'w') as f:
         dict_writer = csv.DictWriter(f, output_ntrs_cdms_results[0].keys())
