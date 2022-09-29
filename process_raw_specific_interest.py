577042fb43b40dd5a6dc18c4c029e5b8ddf42dde
joseph.cheng <joseph.cheng@bp.com>
2022-09-30 09:34:33 +1000

    refactor and process csv for scope and interest file

--- process_raw_specific_interest.py
+++ process_raw_specific_interest.py
@@ -8,7 +8,7 @@ with open("data/raw_file/raw_nasa_scope_input_subjects_of_specific_interest.txt"
         category_id.append(int(line.split('.')[-1].strip()))
 
     header = ['interest','category_id']
-    with open("data/nasa_scope_subject_category_to_interest.csv", 'w') as f:
+    with open("data/nasa_scope_category_to_interest.csv", 'w') as f:
         writer = csv.writer(f)
         writer.writerow(header)
         writer.writerows(zip(interest, category_id))
