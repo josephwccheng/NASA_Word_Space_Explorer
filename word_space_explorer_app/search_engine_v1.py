d7f0f461de5595bb032c712dc2139e57d86609b1
joseph.cheng <joseph.cheng@bp.com>
2022-10-02 00:42:18 +1000

    foundational flask build

---
+++ word_space_explorer_app/search_engine_v1.py
@@ -0,0 +1,40 @@
+# Used for Progress Bar
+from tqdm import tqdm
+import pandas as pd
+
+from word_embedder_v1 import WordEmbedderV1
+
+class SearchEngineV1:
+    def __init__(self, glove_embedder_file_path:str= 'model/glove.6B.100d.txt', document_embedder_file_path:str='data/ntrs_word_embedder_with_glove.txt'):
+        self.glove_search_engine_v1 = WordEmbedderV1(embedder_file_path=glove_embedder_file_path)
+        self.document_search_engine_v1 = WordEmbedderV1(embedder_file_path=document_embedder_file_path)
+
+    def recommend_top_documents_from_query(self, input_text:str, top_n:int=10):
+        emmbedes = self.glove_search_engine_v1.get_sum_array_embedding(input_text)
+        document_id_list = self.document_search_engine_v1.find_similar_word_from_glove_vector(emmbedes)
+        return document_id_list[:top_n]
+
+
+if __name__ == '__main__':
+    # Example of how Search Engine is loaded and run
+    glove_embedder_file_path= 'model/glove.6B.100d.txt'
+    document_embedder_file_path='data/ntrs_word_embedder_with_glove.txt'
+    search_engine_v1 = SearchEngineV1(glove_embedder_file_path=glove_embedder_file_path, document_embedder_file_path=document_embedder_file_path)
+
+    test_string = 'Preliminary Results of Flight Tests of a Conventional Three-Blade Propeller at High Speeds'
+    recommended_document_list = search_engine_v1.recommend_top_documents_from_query(test_string)
+
+    # (Optional) Validate the Recommder
+    input_ntrs_cdms_results_path = "data/ntrs_cdms_results.csv"
+    ntrs_cdms_results_df = pd.read_csv(input_ntrs_cdms_results_path)
+    validate_list = []
+    count = 0
+    for document_id, title, abstract in tqdm(zip(ntrs_cdms_results_df.document_id.tolist(),ntrs_cdms_results_df.title.tolist(), ntrs_cdms_results_df.abstract.tolist())):
+        recommended_document_list = search_engine_v1.recommend_top_documents_from_query(title)
+        # recommended_document_list = search_engine_v1.recommend_top_documents_from_query(title + " " + abstract)
+        if str(document_id) in recommended_document_list:
+            validate_list.append(recommended_document_list.index(str(document_id)))
+        count = count + 1
+        if count > 100:
+            break
+    print("test")
---
+++ word_space_explorer_app/word_embedder_v1.py
@@ -0,0 +1,63 @@
+# Prerequisit Download GloVe Model
+# !wget http://nlp.stanford.edu/data/glove.6B.zip
+# !unzip -q glove.6B.zip
+# tutorial: https://keras.io/examples/nlp/pretrained_word_embeddings/
+# https://analyticsindiamag.com/hands-on-guide-to-word-embeddings-using-glove/
+# https://www.analyticsvidhya.com/blog/2019/07/how-get-started-nlp-6-unique-ways-perform-tokenization/#:~:text=NLTK%20contains%20a%20module%20called,document%20or%20paragraph%20into%20sentences
+
+import numpy as np
+from scipy import spatial
+from gensim.parsing.preprocessing import remove_stopwords
+# Using Keras to perform tokenisation as it does to_lower and removes unnecessary punctuations
+# Note: tokenize function has to be created to remove - from the reggex filter list because glove uses it
+import tensorflow as tf
+
+class WordEmbedderV1:
+    def __init__(self, embedder_file_path:str= 'model/glove.6B.100d.txt'):
+        self.embeddings_index = self.load_glove_word_embedding(embedder_file_path)
+    
+    # Load the pre-trained word embeddings and make a dict mapping words to their numpy vector representation
+    def load_glove_word_embedding(self, embedder_file_path:str):
+        embeddings_index = {}
+        with open(embedder_file_path) as f:
+            for line in f:
+                word, coefs = line.split(maxsplit=1)
+                coefs = np.fromstring(coefs, "f", sep=" ")
+                embeddings_index[word] = coefs
+        print("Word Embedder created with %s word vectors." % len(embeddings_index))
+        return embeddings_index
+
+    def get_sum_array_embedding(self, input_text:str):
+        result_array = []
+        unique_tokenised_text = set(self.tokenize(input_text))
+        # analysis_total = len(unique_tokenised_text)
+        # analysis_not_found = 0
+        for word in unique_tokenised_text:
+            if word in self.embeddings_index.keys():
+                result_array.append(self.embeddings_index[word])
+            else:
+                # TODO - Track the words that couldnt be captured from the GloVe model -> i.e. print(f'{word} was not found in the glove model')
+                # analysis_not_found = analysis_not_found + 1
+                continue
+        # print(f'not found percentage is {analysis_not_found/analysis_total}')
+        return sum(result_array)
+
+    def find_similar_word_from_glove_vector(self, emmbedes, distance_algo='cosine'):
+        if distance_algo == 'euclidean':
+            nearest = sorted(self.embeddings_index.keys(), key=lambda word: spatial.distance.euclidean(self.embeddings_index[word], emmbedes))
+        else:
+            nearest = sorted(self.embeddings_index.keys(), key=lambda word: spatial.distance.cosine(self.embeddings_index[word], emmbedes))
+        return nearest
+
+    # Helper Functions
+    def get_embeddings_index(self):
+        return self.embeddings_index
+
+    def tokenize(self, input_text:str):
+        filtered_text = remove_stopwords(input_text)
+        return tf.keras.preprocessing.text.text_to_word_sequence(
+            filtered_text,
+            filters='!"#$%&()*+,./:;<=>?@[\\]^_`{|}~\t\n',
+            lower=True,
+            split=' '
+        )
